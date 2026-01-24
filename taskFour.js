9e10775a3c29aef76e624dc081e879fe405eb9fa
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-24 22:40:48 +0600

    task done

---
+++ taskFour.js
@@ -0,0 +1,13 @@
+// ++++++++++++++Task-04++++++++++++++++++++++
+
+// What will be the result of the following codes:
+
+ var ab = isNaN(‘11’);
+//  true
+
+var a = isNaN(2-10);
+// false
+
+
+// Explain your answers. it's a built in function in js to find out whather this is number or not. if it is number then it return false output and if it is not a number then it show true.
+
