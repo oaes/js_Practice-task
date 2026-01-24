280d37f9d287e2a998c5478b87161cb973dc3e9f
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-24 21:26:09 +0600

    js practice task

---
+++ taskOne.js
@@ -0,0 +1,7 @@
+//.......Task-1............ You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// Input:
+// The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+// Output:
+// Print the result.
