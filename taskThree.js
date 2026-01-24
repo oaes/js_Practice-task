81804e326ca857917bf2ec751f661004329af6a6
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-24 22:25:07 +0600

    task done

---
+++ taskThree.js
@@ -0,0 +1,21 @@
+// +++++++++++++++task Three++++++++++++++++++++
+
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+
+// Sample Output:
+// 4
+
+var sampleNumber = 119;
+var divideNumber = 119 % 5;
+
+console.log(sampleNumber);
+console.log(divideNumber);
