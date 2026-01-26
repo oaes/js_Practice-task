2e9f822126049fc917efb07ccfff92e99d660b69
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-26 21:07:50 +0600

    logic added

--- if.js
+++ if.js
@@ -2,3 +2,10 @@ let foodPrice = 60;
 if (foodPrice < 70) {
   console.log("i will buy one kilo");
 }
+
+let player = 9;
+if (player > 12) {
+  console.log("wont play");
+} else {
+  console.log("i will play");
+}
