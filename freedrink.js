cae0d8b7eaa385ae667c58fad3b9aa0877cf3c73
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-27 20:58:47 +0600

    if task 1 done

---
+++ freedrink.js
@@ -0,0 +1,13 @@
+/**
+ *Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+ */
+
+let burger = 500;
+
+if (burger < 545) {
+  console.log("free coke");
+} else {
+  console.log("30tk coke");
+}
