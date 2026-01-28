c03aaad5cfa8d88fc0d93e057d1d2c040af0a526
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-28 22:38:54 +0600

    added new file

---
+++ array2.js
@@ -0,0 +1,8 @@
+let rollNumber = [3, 4, 5, 6, 7, 8, 9, 11, 12];
+console.log(rollNumber);
+
+rollNumber.push(13, 14, 15, 16);
+rollNumber.pop();
+rollNumber.shift();
+rollNumber.unshift(1, 2, 3);
+console.log(rollNumber);
