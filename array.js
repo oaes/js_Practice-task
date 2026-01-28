8bee9363fa4474884057e3f94fde63a6adde3ce1
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-28 22:29:49 +0600

    array.js file addded.

---
+++ array.js
@@ -0,0 +1,11 @@
+let fruitNumber = [44, 443, 32, 432, 22, 322, 99];
+let friendName = ["bablu, arshad, mobin, sam, yusuf, rafiq"];
+// console.log(friendName);
+// console.log(fruitNumber);
+// console.log(fruitNumber.length); for knowing length of the array
+// console.log(fruitNumber[4]); for knowing the index of the array and you can also change the array value by calling their index.
+
+const newNm = fruitNumber[4];
+console.log(newNm);
+fruitNumber[4] = 50;
+// console.log(fruitNumber);
