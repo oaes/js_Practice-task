b1dde0db94443fc76a6cf4de08507bce9b120030
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-29 21:06:24 +0600

    for loop concept revised

---
+++ for.js
@@ -0,0 +1,9 @@
+const fruits = ["lemon", "mango", "berry", "banana", "kiwi"];
+console.log(fruits);
+
+// for loop
+
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("i like kiwi");
+}
