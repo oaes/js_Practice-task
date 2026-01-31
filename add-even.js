3e96be8ecc840b7c80a2cd4a94971abe301fb86d
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-01-31 20:13:03 +0600

    new file and code added

---
+++ add-even.js
@@ -0,0 +1,10 @@
+// odd numbers
+for (let numbers = 1; numbers < 50; numbers += 2) {
+  console.log("odd numbers", numbers);
+}
+
+// even numbers
+
+for (let i = 0; i < 20; i = i + 2) {
+  console.log("even numbers", i);
+}
