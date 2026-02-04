de57f6c7fd83d801224d3a14453c6bceb72ebae4
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-02-04 22:37:24 +0600

    object file and code added

---
+++ object.js
@@ -0,0 +1,18 @@
+const myPhone = {
+  name: "iqoo z9s",
+  ram: 8,
+  display: 6.2,
+  androidVersion: 14,
+  fontCamera: "15px",
+  rearCamera: "50px",
+  service: true,
+  features: ["curved display", "handly", "good camera", "nice color"],
+};
+console.log(myPhone);
+
+myPhone.ram = "8gb";
+myPhone.androidVersion = "14oSe";
+myPhone.features = ["easy to use", "gifted"];
+myPhone.service = false;
+
+console.log(myPhone);
