3111288ac347c1a1fef1bd06445a05f9fdb561fc
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-02-04 21:01:16 +0600

    string reverse

---
+++ different.js
@@ -0,0 +1,11 @@
+const student = "esa, sadia, sara, soto sadia";
+console.log(student);
+console.log(typeof student);
+
+const sentence = "i want money";
+
+let reverse = "";
+for (const letter of sentence) {
+  reverse = letter + reverse;
+}
+console.log(reverse);
