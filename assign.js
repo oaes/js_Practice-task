88502d754f7630fa5be014ded4e7588e280b3f36
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-02-07 12:49:52 +0600

    first pblm solve

--- assign.js
+++ assign.js
@@ -3,18 +3,19 @@
 /*function signature/sample */
 function newPrice(currentPrice, discount) {
   // You have to write your code here
-  // if (typeof currentPrice !== "number" || discount !== "number") {
-  //   ("Invalid");
-  // } else if (discount < 0 && discount > 100) {
-  //   ("Invalid");
-  //   let count = (currentPrice * discount) / 100;
-  //   let final = currentPrice - count;
-  //   return final.toFixed(3);
-  // }
+  if (typeof discount !== "number" || typeof currentPrice !== "number") {
+    return "Invalid";
+  } else if (discount > 100 || discount < 0) {
+    return "Invalid";
+  } else {
+    let count = (currentPrice * discount) / 100;
+    let final = currentPrice - count;
+    return final.toFixed(3);
+  }
 
-  let count = (currentPrice * discount) / 100;
-  let final = currentPrice - count;
-  return final.toFixed(3);
+  // let count = (currentPrice * discount) / 100;
+  // let final = currentPrice - count;
+  // return final.toFixed(3);
 }
 
-console.log(newPrice(2000, 15));
+console.log(newPrice(1000, 20));
