5306db53248740e31b619bd08314289531e794bc
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-02-28 16:19:44 +0600

    new file added

---
+++ backtic.js
@@ -0,0 +1,4 @@
+// Template literals also known as backtic which is ` `. it is very powerful for writing multiline strings and dynamic strings usually.
+
+let name = `one, two, three, four,five, six`;
+console.log(name);
