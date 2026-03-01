a53365669512dba51815b2862361d0fa182eaa4c
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-03-01 07:14:54 +0600

    done

---
+++ spread.js
@@ -0,0 +1,7 @@
+// ...spread operator for copying array or object
+
+const name = ["akash", "arshad", "bablu", "rafiq", "jamal"];
+const friendName = [...name];
+friendName.push("kamrul");
+console.log(friendName);
+console.log(name);
